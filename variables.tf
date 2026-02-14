d1958f0b2ffffdcbe241936364d4f51c638fa5cd
Shalini GR <165385782+ShaliniGR05@users.noreply.github.com>
2026-02-14 11:41:53 +0530

    Refactor variables in variables.tf
    Removed descriptions from variables and added validation for ami_id.

--- variables.tf
+++ variables.tf
@@ -1,29 +1,24 @@
 variable "aws_region" {
-  description = "AWS region where resources will be created"
-  type        = string
-  default     = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
-
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
+  type    = string
+  default = "t3.micro"
 }
-
 variable "ami_id" {
-  description = "AMI ID for EC2 instance (Ubuntu for us-east-1)"
-  type        = string
-  default     = "ami-0b6c6ebed2801a5cb"
-}
+  type = string
 
+  validation {
+    condition     = length(trimspace(var.ami_id)) > 0
+    error_message = "ami-0b6c6ebed2801a5cb"
+  }
+}
 variable "existing_key_pair_name" {
-  description = "Existing AWS key pair name"
-  type        = string
-  default     = "devops"
+  type    = string
+  default = "devops"
 }
-
 variable "allowed_ssh_cidr" {
-  description = "CIDR block allowed to SSH into EC2"
-  type        = string
-  default     = "0.0.0.0/0"
+  type    = string
+  default = "0.0.0.0/0"
 }
